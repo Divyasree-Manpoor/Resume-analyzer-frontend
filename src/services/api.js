3b56736bfdc0e5a7303bf1ea06c1ae49a4ecd7ef
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2026-06-15 20:44:41 +0530

    debug api

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
+console.log(
+  "API BASE URL:",
+  "https://resume-analyzer-backend-74ic.onrender.com/api"
+);
+
 const API = axios.create({
   baseURL: "https://resume-analyzer-backend-74ic.onrender.com/api",
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default API;

afda006e654df4f963f05dc1e3d05362166b9111
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2026-06-15 19:52:54 +0530

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://resume-analyzer-backend-74ic.onrender.com/" ,
+  baseURL: "https://resume-analyzer-backend-74ic.onrender.com/api",
   // baseURL:  "http://localhost:9000/api",
 });
 
 API.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
